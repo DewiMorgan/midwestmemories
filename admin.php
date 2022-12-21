30bf9614f8e1ed85a866ba9450798b486414108a
DewiMorgan <dewi@dewimorgan.com>
2022-12-20 22:36:26 -0600

    fix list type

--- admin.php
+++ admin.php
@@ -50,10 +50,10 @@ $db->sqlExec(
     $cursor = $_REQUEST['cursor'] ?? null;
     $fp = new InitDropbox();
     if ($cursor) {
-        echo "<h2>Full List</h2>\n<pre>";
+        echo "<h2>Updates from cursor</h2>\n<pre>";
         $list = $fp->getUpdates($client, $cursor);
     } else {
-        echo "<h2>Updates</h2>\n<pre>";
+        echo "<h2>Full List</h2>\n<pre>";
         $list = $fp->getRecursiveList($client);
     }
     var_export($list);
