dafd6e267f4b871d2f8882db541dc1c47b4dc8d6
Dewi Morgan <dewi@dewimorgan.com>
2023-06-18 15:54:05 -0500

    testing various use paths

--- index.php
+++ index.php
@@ -5,7 +5,14 @@ declare(strict_types=1);
 
 namespace MidwestMemories;
 
-use app\MidwestMemories\Index;
+use app\test1;
+use \app\test2;
+use MidwestMemories\test3;
+use \MidwestMemories\test4;
+use \test5;
+use test6;
+// This is where I want it to be.
+use MidwestMemories\Index;
 
 date_default_timezone_set('US/Central');
 session_start();
