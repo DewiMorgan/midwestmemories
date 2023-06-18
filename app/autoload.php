273980c8b60b75bf00c739b0c39742b66abdc618
Dewi Morgan <dewi@dewimorgan.com>
2023-06-18 15:18:44 -0500

    Try ot make autoloader entirely automagic.

--- app/autoload.php
+++ app/autoload.php
@@ -6,6 +6,9 @@ declare(strict_types=1);
 require_once __DIR__ . '/../vendor/autoload.php';
 
 // Then our own.
+spl_autoload_extensions('.php'); // comma-separated list
+spl_autoload_register();
+/*
 spl_autoload_register(function (string $class): void {
     file_put_contents('./autoload.out', "Looking for '$class'\n", FILE_APPEND);
     $file = str_replace('\\', '/', $class) . '.php';
@@ -17,3 +20,4 @@ spl_autoload_register(function (string $class): void {
         file_put_contents('./autoload.out', "404: File not found!\n", FILE_APPEND);
     }
 });
+    */
