c7a89c0d15247350b2bdb6c8b8d7773d0530e6f8
Dewi Morgan <dewi@dewimorgan.com>
2023-06-18 15:10:42 -0500

    Add logging to autoloader.

--- app/autoload.php
+++ app/autoload.php
@@ -7,8 +7,13 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 // Then our own.
 spl_autoload_register(function (string $class): void {
+    file_put_contents('./autoload.out', "Looking for '$class'\n", FILE_APPEND);
     $file = str_replace('\\', '/', $class) . '.php';
+    file_put_contents('./autoload.out', "File is '$file'\n", FILE_APPEND);
     if (file_exists($file)) {
+        file_put_contents('./autoload.out', "File found and included!\n", FILE_APPEND);
         include $file;
+    } else {
+        file_put_contents('./autoload.out', "404: File not found!\n", FILE_APPEND);
     }
 });
