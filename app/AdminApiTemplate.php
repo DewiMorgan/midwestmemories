af2e31f727869edca734e103098266b3f9bd8e64
Dewi Morgan <git@github.com>
2025-06-08 08:35:52 -0500

    Move frontend listUsers to use new API. typo users->user #2

--- app/AdminApiTemplate.php
+++ app/AdminApiTemplate.php
@@ -495,7 +495,7 @@
         const params = new URLSearchParams(window.location.search);
 
         // Populate the user list.
-        listUsers('/api/v1.0/users');
+        listUsers('/api/v1.0/user');
 
         // Only if requested by the user.
         if ("handle_init_root" === params.get("user-action")) {
