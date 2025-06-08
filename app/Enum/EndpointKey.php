74f8fbb77d4d93c0c7c52b49cd9006d416b55af2
Dewi Morgan <git@github.com>
2025-06-07 23:05:22 -0500

    Separate out API, trying to make EndpointKey valid PHP, #12

--- app/Enum/EndpointKey.php
+++ app/Enum/EndpointKey.php
@@ -3,25 +3,23 @@
 declare(strict_types=1);
 
 namespace MidwestMemories\Enum;
-require_once('HttpMethod.php');
-require_once('EndpointPath.php');
 
 /**
  * Enum representing all API endpoints in the system.
  */
 enum EndpointKey: string
 {
-    case DELETE_COMMENT = HttpMethod::DELETE->value . '#' . EndpointPath::COMMENT->value;
-    case GET_COMMENT = HttpMethod::GET->value . '#' . EndpointPath::COMMENT->value;
-    case GET_CURSOR = HttpMethod::GET->value . '#' . EndpointPath::CURSOR->value;
-    case GET_DOWNLOAD = HttpMethod::GET->value . '#' . EndpointPath::DOWNLOAD->value;
-    case GET_PROCESS = HttpMethod::GET->value . '#' . EndpointPath::PROCESS->value;
-    case GET_USER = HttpMethod::GET->value . '#' . EndpointPath::USER->value;
-    case POST_COMMENT = HttpMethod::POST->value . '#' . EndpointPath::COMMENT->value;
-    case POST_CURSOR = HttpMethod::POST->value . '#' . EndpointPath::CURSOR->value;
-    case POST_DOWNLOAD = HttpMethod::POST->value . '#' . EndpointPath::DOWNLOAD->value;
-    case POST_PROCESS = HttpMethod::POST->value . '#' . EndpointPath::PROCESS->value;
-    case POST_USER = HttpMethod::PUT->value . '#' . EndpointPath::USER->value;
-    case PUT_COMMENT = HttpMethod::PUT->value . '#' . EndpointPath::COMMENT->value;
-    case PUT_USER = HttpMethod::POST->value . '#' . EndpointPath::USER->value;
+    case DELETE_COMMENT = 'DELETE#comment';
+    case GET_COMMENT = 'GET#comment';
+    case GET_CURSOR = 'GET#cursor';
+    case GET_DOWNLOAD = 'GET#download';
+    case GET_PROCESS = 'GET#process';
+    case GET_USER = 'GET#user';
+    case POST_COMMENT = 'POST#comment';
+    case POST_CURSOR = 'POST#cursor';
+    case POST_DOWNLOAD = 'POST#download';
+    case POST_PROCESS = 'POST#process';
+    case POST_USER = 'PUT#user';
+    case PUT_COMMENT = 'PUT#comment';
+    case PUT_USER = 'POST#user';
 }
