527fbfaef120b63218cc76ab7a31416f6d0ae047
Dewi Morgan <git@github.com>
2025-07-05 04:37:04 -0500

    Try to fix UserTable. #13

--- raw/admin.js
+++ raw/admin.js
@@ -437,6 +437,7 @@ console.log(newRow); // Should be a <tr>
 console.log(newRow.outerHTML); // Check structure
 console.log(UserTable.table.tBodies[0].innerHTML); // Before and after insertion
 console.log(UserTable.table.innerHTML); // See if the row ends up somewhere unexpected
+console.log(document.getElementById('user-list').innerHTML); // See if the row ends up somewhere unexpected
         } else {
             console.log("Appending row", newRow);
             tbody.append(newRow);
