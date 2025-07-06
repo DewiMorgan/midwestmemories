a9dd64e8728bc6e5e52377dda981ee956c73993d
Dewi Morgan <git@github.com>
2025-07-05 22:02:00 -0500

    Fix version strings in API calls

--- raw/user.js
+++ raw/user.js
@@ -92,7 +92,7 @@ window.Comments = class {
         let totalPages = 1; // Start assuming only 1 page until we know otherwise.
 
         do {
-            const response = await fetch(`/api/v1/comment/${fileId}/${currentPage}`);
+            const response = await fetch(`/api/v1.0/comment/${fileId}/${currentPage}`);
             if (!response.ok) {
                 throw new Error(`Failed to fetch page ${currentPage}: ${response.statusText}`);
             }
@@ -120,7 +120,7 @@ window.Comments = class {
      */
     async postComment(bodyText) {
         const fileId = this.getFileId();
-        const response = await fetch(`/api/v1/comment/${fileId}`, {
+        const response = await fetch(`/api/v1.0/comment/${fileId}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
