d410339903de1f481b76fc52ce7831bf65aec8dc
Dewi Morgan <git@github.com>
2025-07-07 23:42:41 -0500

    Debug to find what I broke about the templates. #3

--- src/TreeTemplate.php
+++ src/TreeTemplate.php
@@ -67,9 +67,9 @@ $isLiveSite = str_contains(__DIR__, 'midwestmemoriesfamily');
          */
         function scanDirectory(string $scanUnixDir, string $targetUnixPath = ''): void
         {
-Log::debug("scanDirectory in $scanUnixDir", $targetUnixPath);
             $items = scandir($scanUnixDir);
 Log::debug('Before sort', $items);
+Log::debug('Checking that Path class is accessible', Path::unixPathToUrl(''));
             // Sort to natural order, with directories first.
             usort($items, [Path::class, 'sortFolder']);
 Log::debug('After sort', $items);
